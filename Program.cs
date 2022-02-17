9105e895d5a852da23f28c8a36519481925472e6
Joshua Smith <jgs73@live.co.uk>
2022-02-17 21:49:33 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringReversal // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "";
+            string reversed_value = "";
+            Console.Write("Enter a string here: ");
+            value = Console.ReadLine();
+            reversed_value = new string(value.Reverse().ToArray());
+            Console.WriteLine(reversed_value);
+        }
+    }
+}
